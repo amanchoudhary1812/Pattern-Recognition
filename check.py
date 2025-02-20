a094394d8d1e07fcdd390a7de4c7a419895b37d1
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-02-20 23:21:54 +0530

    Commit

---
+++ check.py
@@ -0,0 +1,18 @@
+# Core Libraries
+import sys
+import os
+import math
+import random
+import time
+import datetime
+
+# Data Science & ML Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import tensorflow as tf
+
+# Check TensorFlow Version
+print("✅ All libraries imported successfully!")
+# print(f"🔹 TensorFlow Version: {tf.__version__}")
